a45f1dd10cbdad3103b5f3eb14e249f95f7fe4ea
hoe-rake <ludoludo@outlook.jp>
2023-12-30 22:23:25 +0900

    :construction: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import { SITE_URL } from "./src/consts.ts";
+import { REPOSITORY, SITE_URL } from "./src/consts.ts";
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  base: '/lemma',
   integrations: [
     mdx(),
     sitemap(),
