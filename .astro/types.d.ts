f598b3e56ec2354c014211e4e7665e75b4402508
hoe-rake <ludoludo@outlook.jp>
2024-01-12 23:17:26 +0900

    :memo: add dotfiles

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -194,6 +194,13 @@ declare module 'astro:content' {
                 collection: 'blog'
                 data: InferEntrySchema<'blog'>
             } & { render(): Render['.md'] }
+            '20240112.md': {
+                id: '20240112.md'
+                slug: '20240112'
+                body: string
+                collection: 'blog'
+                data: InferEntrySchema<'blog'>
+            } & { render(): Render['.md'] }
             'first-post.md': {
                 id: 'first-post.md'
                 slug: 'first-post'
